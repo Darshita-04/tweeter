0b85b67b547d861be1581c8b91ecbeba1349b14a
Darshita Patel <pateldarshita1994@gmail.com>
2024-11-18 04:05:35 -0500

    character counter implemented

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function(){   
+    
+    const counter = $(this).siblings(".compose-tweet-footer").find(".counter");
+    const btnTweet = $(this).siblings(".compose-tweet-footer").find(".btn-tweet");
+    const inputLength = $(this).val().length; // get length of input value     
+    const maxLength = 140; // max character limit
+    const remainingChar = maxLength - inputLength;
+    counter.text(remainingChar);
+    // highlight if char remaining is in -ve
+    if (remainingChar < 0) {
+      counter.addClass("error");
+      btnTweet.attr("disabled",true);
+    } else {
+      counter.removeClass("error");
+      btnTweet.attr("disabled",false);
+    }
+  })
+});
